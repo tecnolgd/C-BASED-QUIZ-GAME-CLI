adb57170e5378965783201021e26d33af8f78328
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-05-18 07:50:29 +0530

    updated sub for source code

--- rules.c
+++ rules.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include "head.h"
 void showRules(){
-    printf("the rules for the game as follows:");
+    printf("the rules for the game as follows:\n");
+    printf("Number of Questions\nhe quiz consists of a fixed number of questions (e.g., 5 or 10), each with multiple choice answers (A, B, C, D)");
+printf("Scoring\nEach correct answer awards 1 point.\nThere is no negative marking for wrong answers.");
+printf("\nYour total score will be displayed at the end of the quiz\n");
+printf("Answer\nFormat:\n");printf("You must input only the option letter (A, B, C, or D).\n");printf("Answers are case-insensitive (both a and A are valid).\n");printf("No Second Chances:\nOnce you answer a question, you cannot change your response.\nTime Limit (Optional Feature):\n");printf("If implemented, each question must be answered within a certain time limit.\nVictory Condition:\nA minimum score (e.g., 70 per or 4 out of 5) may be required to the game.\nBonus messages or rankings can be displayed based on performance.\nCheating Not Allowed\nThis quiz relies on your honesty. Dont peek at answers only cheating yourself 😉.\nExit Option:\nYou can quit the quiz at any time by choosing the exit option in the menu.");
+
     
 }
