21526b1a0a5dbaa2694d7ceaa38f131e4710968e
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-05-19 21:08:13 +0530

    updated sub file of source code

--- question_rank.c
+++ question_rank.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include<string.h>
+#include<conio.h>
 #include "head.h"
 void startQuiz(){
     
@@ -8,16 +9,17 @@ void startQuiz(){
      
     int score=0;
     char choice_options[4];
-    char questions[3]={"1.what is the capital of france?","2.who created c programming language?","3.who is the father of computers?"};
-    char options[4][3]={{"1.berlin","2.tokyo","3.paris","4.dublin"},{"1.dennis ritchie","2.b.jarne stroustrup","3.larry page","4.bill gates"},{"1.charles babbage","2.bengamin franklin","3.lady ada lovelace","4.elon musk"}};
-    char correct_options[3]={"3","1","1"};
+    unsigned char questions[4]={"1.what is the capital of france?","2.who created c programming language?","3.who is the father of computers?"};
+    char options[5][4][20]={{"1.berlin","2.tokyo","3.paris","4.dublin"},{"1.dennis ritchie","2.b.jarne stroustrup","3.larry page","4.bill gates"},{"1.charles babbage","2.bengamin franklin","3.lady ada lovelace","4.elon musk"}};
+    char correct_options[3]={'3','1','1'};
     printf("the quiz has started..\n");
     while(i<4){
-        printf("%s\n",questions[i]);
+        printf("%ls\n",questions[i]);
         for(int i=0;i<4;i++){
             for( j=0;j<5;j++){
-         printf("%s\n",options[i][j]);
-         
+                int k=0;
+         printf("%s\n",options[i][j][k]);
+         k++;
         scanf("%s",&choice_options[j]);
              } }
         
