a1f55bb84b59d5d46c4d7b76c0cf8c9b88ff9431
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-05-20 19:51:51 +0530

    updated sub file of source code

--- question_rank.c
+++ question_rank.c
@@ -4,38 +4,40 @@
 #include<conio.h>
 #include "head.h"
 void startQuiz(){
-    
-    int j,i=0;
      
     int score=0;
-    char choice_options[3];
+    char choice_option;
     char questions[3][100]={"1.what is the capital of france?","2.who created c programming language?","3.who is the father of computers?"};
-    char options[3][4][100]={{"1.berlin","2.tokyo","3.paris","4.dublin"},{"1.dennis ritchie","2.b.jarne stroustrup","3.larry page","4.bill gates"},{"1.charles babbage","2.bengamin franklin","3.lady ada lovelace","4.elon musk"}};
-    char correct_options[3]={'3','1','1'};
-    printf("the quiz has started..\n");
+    char options[3][4][100]={{"a.berlin","b.tokyo","c.paris","d.dublin"},{"a.dennis ritchie","b.b.jarne stroustrup","c.larry page","d.bill gates"},{"a.charles babbage","b.bengamin franklin","c.lady ada lovelace","d.elon musk"}};
+    char correct_options[3]={'c','a','a'};
+    printf("the quiz starts..\n");
     
         
         for(int i=0;i<3;i++){
             printf("%s\n",questions[i]);
-            for( j=0;j<4;j++){
+            for( int j=0;j<4;j++){
                 
          printf("%s\n",options[i][j]);
-            }
-        scanf("%c\n",&choice_options[j]);
-         
-        int k=0;
-    
+            } 
+            while ((getchar()) != '\n');
+        scanf("%c",&choice_option);
+            
        
-    if(choice_options[j]==correct_options[j]){
+    
+
+    if(choice_option==correct_options[i]){
+        
         printf("correct!\n");
         score=score+20;
     }
     else {
     printf("wrong\n");
     score=score+0;
-    }
-    k++;
-       }
+    
+}
+
+        }
+       
 
    if (score >= 80) {
     printf("Rank: S - high knowledge \n");
