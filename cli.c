da829ca14437439bb073db5e048cb3daecfe0b2a
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-05-31 18:45:54 +0530

    updated sub file of source code

--- cli.c
+++ cli.c
@@ -1,3 +1,12 @@
-#include <stdio.h>
 
+#include "head.h"
+
+
+int displayMenu(){
+    printf("\t**QUIZZER RUSH**\t\n");
+    printf("1. start quiz\n");
+    printf("2. view rules\n");
+    printf("3. exit\n");
+    
+}
 
