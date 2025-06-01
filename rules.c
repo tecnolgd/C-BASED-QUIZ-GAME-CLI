31c2e373fefc4c5854369dcbe217458f05aafa28
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-06-01 20:08:39 +0530

    updated sub file of source code

--- rules.c
+++ rules.c
@@ -1,16 +1,8 @@
 #include "head.h"
 
-
 void showRules(){
     
-    
+    printf("**The rules for the game as follows:\nNumber of Questions:\n\tThe quiz consists of 5 questions,each with multiple choice answers (a,b,c,d)\nScoring:\n\tEach correct answer awards 1 point.\n\tThere is no negative marking for wrong answers.\n\tYour total score will be displayed at the end of the quiz\nAnswer Format:\n\tYou must input only the option letter (a,b,c or d).\n\tAnswers are case-insensitive (both a and A are valid).\nNo Second Chances:\n\tOnce you answer a question, you cannot change your response.\nVictory Condition:\n\tA minimum score (e.g., 70 per or 4 out of 5) may be required to win the game.\n\tBonus messages or rankings can be displayed based on performance.\n**Cheating Not Allowed:\n\tThis quiz relies on your HONESTY. \n\tDont peek at answers.\n\tcheating yourself is the biggest downfall .\nExit Option:\n\tYou can quit the quiz at any time by choosing the exit option in the menu.");
+    printf("\n______________________________________________________");
 
-    printf("the rules for the game as follows:\n");
-    printf("Number of Questions\nhe quiz consists of a fixed number of questions (e.g., 5 or 10), each with multiple choice answers (A, B, C, D)");
-printf("Scoring\nEach correct answer awards 1 point.\nThere is no negative marking for wrong answers.");
-printf("\nYour total score will be displayed at the end of the quiz\n");
-printf("Answer\nFormat:\n");printf("You must input only the option letter (A, B, C, or D).\n");printf("Answers are case-insensitive (both a and A are valid).\n");printf("No Second Chances:\nOnce you answer a question, you cannot change your response.\nTime Limit (Optional Feature):\n");printf("If implemented, each question must be answered within a certain time limit.\nVictory Condition:\nA minimum score (e.g., 70 per or 4 out of 5) may be required to the game.\nBonus messages or rankings can be displayed based on performance.\nCheating Not Allowed\nThis quiz relies on your honesty. Dont peek at answers.cheating yourself is the biggest downfall .\nExit Option:\nYou can quit the quiz at any time by choosing the exit option in the menu.");
-
-    
 }
-
