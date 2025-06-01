68dddb3c37419348177e2371eb64d4c3c4255518
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-06-01 20:09:32 +0530

    updated sub file of source code

--- quiz.c
+++ quiz.c
@@ -7,65 +7,41 @@
 #include "rules.c"
 
 int back,choice;
- int displayMenu();
+int displayMenu();
 void startQuiz();
 void showRules();
 
 void exitGame(){
-    printf("well play. better next timebye\n");
+    printf("Well play. Better next time.\n");
     printf("/home/\n");
 }
 
-
 int main(){
-  
-  
-  int y;
- 
-    int x;
-   do{
+    do{
 
      
-     choice= displayMenu();
-    
-    printf("enter choice:");
-    scanf("%d",&choice);
-
-    
-   
-    switch(choice){
-        
-        case 1:startQuiz();
-                break;
-            
-        case 2: 
-            showRules();
-          break;
-    
-        
-        case 3: printf("well play.better next time.");
-                exit(0);
+      choice= displayMenu();
+      printf("Enter your choice:");
+      scanf("%d",&choice);
+      
+      switch(choice){
+          case 1: startQuiz();
+                  break;
+          case 2: showRules();
+                  break;
+          case 3: printf("well play.better next time.");
+                  exit(0);
         default: printf("Invalid choice .try again");
        
-                
-        
+      }
+      printf("\nWanna try again??");
+      printf("\n//home//");
+      printf("\nPress 0 to exit ,other number to go to //home// page");
+      scanf("%d",&back);
     }
-    printf("\nwanna try again??");
-    printf("\n//home//");
-
-
-   printf("\npress 0 to exit ,other number to go to home page");
-scanf("%d",&back);
-   }
- while(back!=0);
-
-    printf("well play. better next time.\n");
-
-
-    
-return 0;
-
-
+    while(back!=0);
+    printf("Well play. Better next time.\n");
+    return 0;
 }
 
 
