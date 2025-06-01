36dbcd5dc427a02f417fd32fdaef11235139cc74
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-06-01 20:08:11 +0530

    updated sub file of source code

--- cli.c
+++ cli.c
@@ -1,12 +1,12 @@
-
 #include "head.h"
 
-
 int displayMenu(){
-    printf("\t**QUIZZER RUSH**\t\n");
-    printf("1. start quiz\n");
-    printf("2. view rules\n");
-    printf("3. exit\n");
     
-}
+    printf("\t***QUIZZER RUSH***\t\n");
+    printf("______________________\n");
+    printf("1. Start quiz\n");
+    printf("2. View rules\n");
+    printf("3. Exit\n");
+    printf("______________________\n");
+    }
 
