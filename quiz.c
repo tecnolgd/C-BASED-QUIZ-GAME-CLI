4ca95da0000873fc451a931ad402c962f1578010
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-21 02:48:12 +0000

    fix: minor typos and indentation

--- quiz.c
+++ quiz.c
@@ -1,26 +1,15 @@
 //QUIZZER RUSH-A console menu-based game ,with pure logic of looping statements ,conditions and array logic.
 //Includes conceps of modularisation and linkers in a simple C program.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 #include "head.h"//header file
-#include "question_rank.c"//question logic file
-#include "cli.c" //menu/home page file
-#include "rules.c"//rule set file
 
-int back,choice;
+int back,choice;  //Global variable definitions
+
 int displayMenu();
-void startQuiz();
-void showRules();
-void exitGame(){
-    printf("Well play. Better next time.\n");
-    printf("/home/\n");
-}
 
 int main(){
     do{ //loop for continuos menu display
-        choice= displayMenu();
+        choice = displayMenu();
         printf("Enter your choice:");
         scanf("%d",&choice);
       
@@ -29,31 +18,18 @@ int main(){
                   break;
           case 2: showRules();
                   break;
-          case 3: printf("well play.better next time.");
+          case 3: printf("Well played. Better next time.");
                   exit(0);
-        default: printf("Invalid choice .try again");
+        default: printf("Invalid choice. Try again");
         }
-        printf("\nWanna try again??");
+
+        printf("\nWanna try again?? ");
         printf("\n//home//");
         printf("\nPress 0 to exit ,1 to go to //home// page\t");
         scanf("%d",&back);
     }
     while(back!=0); //condition to check whether user wants to continue the game or not
-    printf("Well play. Better next time.\n");
+    printf("Well played. Better next time.\n");
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-    
-
-        
-
 
