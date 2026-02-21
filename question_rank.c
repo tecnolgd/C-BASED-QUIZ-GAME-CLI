1458c2343fbc99a03390322c0c1565a20aa934cf
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2026-02-21 02:51:08 +0000

    fix: minor typos and indentation

--- question_rank.c
+++ question_rank.c
@@ -2,28 +2,30 @@
 #include "head.h"
 
 void startQuiz(){
-    int score=0;
+
+    int score = 0;
     char choice_option;
-    char questions[3][100]={"1.What is the capital of France?","2.Who created C programming language?","3.Who is the father of computers?"};
-    char options[3][4][100]={{"a.Berlin","b.Tokyo","c.Paris","d.Dublin"},{"a.Dennis Ritchie","b.B.Jarne Stroustrup","c.Larry Page","d.Bill Gates"},{"a.Charles Babbage","b.Benjamin Franklin","c.Lady Ada Lovelace","d.Elon Musk"}};
-    char correct_options[3]={'c','a','a'};
+
+    char questions[3][100] = {"1.What is the capital of France?","2.Who created C programming language?","3.Who is the father of computers?"};
+    char options[3][4][100] = {{"a. Berlin", "b. Tokyo", "c. Paris", "d. Dublin"}, {"a. Dennis Ritchie", "b. B.Jarne Stroustrup", "c. Larry Page", "d. Bill Gates"}, {"a. Charles Babbage", "b. Benjamin Franklin","c. Lady Ada Lovelace", "d. Elon Musk"}};
+    char correct_options[3] = {'c', 'a', 'a'};
+
     printf("The quiz starts..\n");
     
-    for(int i=0;i<3;i++){ //loop for printing questions 
-        printf("%s\n",questions[i]);
-        for( int j=0;j<4;j++){
-            printf("%s\n",options[i][j]);//loop for printing options
+    for(int i=0; i<3; i++){ //loop for printing questions 
+        printf("%s\n", questions[i]);
+        for( int j=0; j<4; j++){
+            printf("%s\n", options[i][j]);//loop for printing options
         } 
         while ((getchar()) != '\n');//removing new line
-        scanf("%c",&choice_option);
+        scanf("%c", &choice_option);
         
-        if(choice_option==correct_options[i]){
-            printf("correct!\n");
-            score=score+20;
+        if(choice_option == correct_options[i]){
+            printf("Correct!\n");
+            score = score + 20;
         }
         else {
-            printf("wrong\n");
-            score=score+0;
+            printf("Wrong\n");
         }
     }
     
